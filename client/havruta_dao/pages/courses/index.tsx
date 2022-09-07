22bff00f65fc01412b57dbff5269db5e6071580a
sungman5 <35528411+sungman5@users.noreply.github.com>
2022-09-08 01:37:58 +0900

    fix : 강의목록 페이지 마크업 업데이트  (#61)
    * add:강의 상세 페이지 화면 마크업  #16
    
    * add: Reply.tsx / UploadReply.tsx, update:[post_id].tsx
    
    * fix: 강의 목록 페이지 #15

--- client/havruta_dao/pages/courses/index.tsx
+++ client/havruta_dao/pages/courses/index.tsx
@@ -1,22 +1,52 @@
-import { PageHeader, Row, Col, Card, Space, Typography, Radio, Tabs } from 'antd';
-import { ThunderboltOutlined } from '@ant-design/icons';
-import Link from 'next/link';
+
+import { PageHeader, Row, Col, Card, Space, Typography, Radio, Tabs, Image } from 'antd';
+import { SearchOutlined, ThunderboltOutlined } from '@ant-design/icons';
+
+const dummyCourses = {
+  title: 'CS지식의 정석 | CS면접 디자인 패턴 네트워크 운영체제 데이터베이스 자료구조 개발자',
+  author: '니콜라스',
+  price: '2',
+  thumbnailURL: '/329248-eng2.png',
+};
 
 export default function details() {
-  const { Title } = Typography;
+  const { Title, Text } = Typography;
   return (
     <section>
       <Space style={{ justifyContent: 'space-between', width: '100%' }}>
-        <PageHeader title="강의탐색" backIcon={true} />
+        <Space>
+          <SearchOutlined style={{ fontSize: '24px' }} />
+          <PageHeader style={{ paddingLeft: 0 }} title="강의탐색" backIcon={true} />
+        </Space>
         <Radio.Group style={{ marginBottom: 8 }} size={'small'}>
           <Radio.Button value="top">최신</Radio.Button>
           <Radio.Button value="left">추천</Radio.Button>
         </Radio.Group>
       </Space>
 
-      <Row gutter={[16, 16]}>
-        <Col style={{ overflow: 'hidden' }} span={8}>
-          <Card
+      <Row gutter={[20, 24]}>
+        <Col span={8}>
+          <Image
+            width={'100%'}
+            height={'auto'}
+            style={{ objectFit: 'cover', marginBottom: '8px', borderRadius:'8px' }}
+            src={`${dummyCourses.thumbnailURL}`}
+          />
+          <Title ellipsis={{rows:2}} level={4} style={{lineHeight:'150%'}} >{dummyCourses.title}</Title>
+          <Space align="center" style={{ justifyContent: 'space-between', width: '100%' }}>
+            <Text style={{ fontSize: '16px' }} type="secondary">
+              {dummyCourses.author}
+            </Text>
+            <Space>
+              <ThunderboltOutlined style={{ fontSize: '16px', paddingTop:'6px', color:'#9b4dea' }} />
+              <Text style={{ fontSize: '16px', color:'#9b4dea', fontWeight:500}} type="secondary">
+                {dummyCourses.price}
+              </Text>
+            </Space>
+          </Space>
+        </Col>        
+      </Row>
+      {/* <Card
             hoverable
             style={{ width: '100%' }}
             cover={
@@ -37,9 +67,7 @@ export default function details() {
                 <span>0.02</span>
               </div>
             </Space>
-          </Card>
-        </Col>
-      </Row>
+          </Card> */}
     </section>
   );
 }
