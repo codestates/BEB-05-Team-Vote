12ea1976e9fc63918586187e0c959f5235fb1d69
yjjjwww <yjjjwww@naver.com>
2022-09-08 02:29:09 +0900

    add: read limited lecture function

--- server/src/router/lecture.ts
+++ server/src/router/lecture.ts
@@ -4,6 +4,7 @@ const {
   postLecture,
   readAllLecture,
   readLectureDetail,
+  readLimitLecture,
 } = require("../controller/lecture_controller");
 
 const router = Router();
@@ -11,5 +12,6 @@ const router = Router();
 router.post("/", postLecture);
 router.get("/", readAllLecture);
 router.get("/detail", readLectureDetail);
+router.get("/limit", readLimitLecture);
 
 module.exports = router;
