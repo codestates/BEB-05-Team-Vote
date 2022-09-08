73588c6c812a382fed01d03f8689ee02ee9c9565
Taehee Yoon <th.yoon.dev@gmail.com>
2022-09-08 15:11:01 +0900

    Revert "add: article CRUD, comment, like function"
    This reverts commit df37ba11280d80f96e95c4ae73556d8535372dc6.

--- server/src/router/like.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-const { postLike } = require("../controller/like_controller");
-
-const router = Router();
-
-router.post("/", postLike);
-
-module.exports = router;
