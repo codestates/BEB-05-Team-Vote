a3f98eddd5200ea914dfaf1b3a0767fb86e89f49
thyoondev <th.yoon.dev@gmail.com>
2022-09-09 03:26:49 +0900

    fix : klaytn type and post interface

--- client/havruta_dao/pages/_app.tsx
+++ client/havruta_dao/pages/_app.tsx
@@ -2,6 +2,38 @@ import type { AppProps } from 'next/app';
 import { LayoutComponent } from '../components/LayoutComponent';
 import '../styles/customTheme.less';
 
+interface Klaytn {
+  on: (eventName: string, callback: () => void) => void;
+  removeListener: (eventName: string, callback: () => void) => void;
+  enable: () => Promise<Array<string>>;
+  selectedAddress: string;
+  networkVersion: number;
+  publicConfigStore: Store;
+  isKaikas: boolean;
+  _kaikas: any;
+}
+
+interface State {
+  isEnabled: boolean;
+  isUnlocked: boolean;
+  networkVersion: number;
+  onboardingcomplete: boolean;
+}
+
+interface Store {
+  subscribe: (callback: () => void) => void;
+  getState: () => State;
+}
+
+declare interface Window {
+  klaytn?: Klaytn;
+}
+declare global {
+  interface Window {
+    klaytn: Klaytn;
+  }
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <LayoutComponent>
