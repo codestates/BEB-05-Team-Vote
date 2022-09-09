780b4e27d815948613fe6469daa8a2b162ea42fa
thyoondev <th.yoon.dev@gmail.com>
2022-09-09 23:43:11 +0900

    add : sentry

---
+++ client/havruta_dao/global.d.ts
@@ -0,0 +1,40 @@
+interface Klaytn {
+  on: (eventName: string, callback: () => void) => void;
+  removeListener: (eventName: string, callback: () => void) => void;
+  enable: () => Promise<Array<string>>;
+  selectedAddress: string;
+  networkVersion: number;
+  publicConfigStore: Store;
+  isKaikas: boolean;
+  _kaikas: any;
+}
+
+interface State {
+  isEnabled: boolean;
+  isUnlocked: boolean;
+  networkVersion: number;
+  onboardingcomplete: boolean;
+}
+
+interface Store {
+  subscribe: (callback: () => void) => void;
+  getState: () => State;
+}
+
+declare interface Window {
+  klaytn?: Klaytn;
+}
+declare global {
+  interface Window {
+    klaytn: Klaytn;
+  }
+}
+
+declare namespace NodeJS {
+  interface Process {
+    env: ProcessEnv;
+  }
+  interface ProcessEnv {
+    [key: string]: string;
+  }
+}
--- client/havruta_dao/next.config.js
+++ client/havruta_dao/next.config.js
@@ -1,15 +1,40 @@
+const { withSentryConfig } = require('@sentry/nextjs');
+const withPlugins = require('next-compose-plugins');
 const withLess = require('next-with-less');
 const path = require('path');
-
 const customTheme = path.resolve('./styles/customTheme.less');
 
-module.exports = withLess({
+const SentryOptions = {
+  sentry: {
+    hideSourceMaps: true,
+  },
+};
+const SentryWebpackPluginOptions = {
+  silent: true,
+};
+
+const WithLessOptions = {
   lessLoaderOptions: {
     lessOptions: {
       additionalData: (content) => `${content}\n\n@import '${customTheme}';`,
     },
   },
+};
+
+const nextConfig = {
+  future: {
+    webpack5: true,
+  },
+  images: {
+    domains: ['havruta.guru'],
+  },
   compiler: {
     styledComponents: true,
   },
-});
+};
+
+module.exports = withSentryConfig(
+  withPlugins([[withLess, { WithLessOptions }]], nextConfig),
+  SentryOptions,
+  SentryWebpackPluginOptions
+);
