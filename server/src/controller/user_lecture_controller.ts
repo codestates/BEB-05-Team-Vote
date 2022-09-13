adc719f6a4b46688d6414f73c9143527269e0751
yjjjwww <yjjjwww@naver.com>
2022-09-13 19:20:03 +0900

    fix : userlecure function from lecture_title to lecture_id

--- server/src/controller/user_lecture_controller.ts
+++ server/src/controller/user_lecture_controller.ts
@@ -3,22 +3,22 @@ const prisma = require("../db/index");
 
 module.exports = {
   postUserLecture: (req: Request, res: Response) => {
-    const { user_id, lecture_title } = req.body;
+    const { user_id, lecture_id } = req.body;
     const postUserLectureHandler = async (
       user_id: Number,
-      lecture_title: String
+      lecture_id: Number
     ) => {
       const checkUserLecture = await prisma.UserLecture.findMany({
         where: {
           user_id: user_id,
-          lecture_title: lecture_title,
+          lecture_id: lecture_id,
         },
       });
       if (checkUserLecture[0] === undefined) {
         await prisma.UserLecture.create({
           data: {
             user_id: user_id,
-            lecture_title: lecture_title,
+            lecture_id: lecture_id,
           },
         });
 
@@ -29,7 +29,7 @@ module.exports = {
         res.status(403).send("userlecture already exists");
       }
     };
-    postUserLectureHandler(user_id, lecture_title).catch(async (e) => {
+    postUserLectureHandler(user_id, lecture_id).catch(async (e) => {
       console.error(e);
       await prisma.$disconnect();
       res.status(500).send("Server Error");
@@ -39,22 +39,22 @@ module.exports = {
   getUserLecture: (req: Request, res: Response) => {
     const user_id: String = req.query.user_id as String;
     const numUser_id = Number(user_id);
-    const lecture_title: String = req.query.lecture_title as String;
+    const lecture_id: Number = Number(req.query.lecture_id as String);
 
     const getUserLectureHandler = async (
       numUser_id: Number,
-      lecture_title: String
+      lecture_id: Number
     ) => {
       const result = await prisma.UserLecture.findMany({
         where: {
           user_id: numUser_id,
-          lecture_title: lecture_title,
+          lecture_id: lecture_id,
         },
       });
       await prisma.$disconnect();
       res.status(201).json(result);
     };
-    getUserLectureHandler(numUser_id, lecture_title).catch(async (e) => {
+    getUserLectureHandler(numUser_id, lecture_id).catch(async (e) => {
       console.error(e);
       await prisma.$disconnect();
       res.status(500).send("Server Error");
