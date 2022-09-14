ae49120a547060b94f4ac4abe3e1debe010ce247
thyoondev <th.yoon.dev@gmail.com>
2022-09-14 15:29:28 +0900

    add : taking class

--- client/havruta_dao/pages/courses/class/[class_id].tsx
+++ client/havruta_dao/pages/courses/class/[class_id].tsx
@@ -1,47 +1,67 @@
-import { PageHeader, Space, Typography, Button } from 'antd';
-import {
-  EyeInvisibleOutlined,
-  EyeOutlined,
-  SearchOutlined,
-  ThunderboltOutlined,
-} from '@ant-design/icons';
+import { PageHeader, Space, Typography } from 'antd';
+import { EditOutlined, RollbackOutlined } from '@ant-design/icons';
+import { GetServerSideProps } from 'next';
+import ReactPlayer from 'react-player/lazy';
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
 
-const dummyCourses = {
-  title: 'CS지식의 정석 | CS면접 디자인 패턴 네트워크 운영체제 데이터베이스 자료구조 개발자',
-  summary:
-    'Java + Spring Boot로 만들어진 웹 애플리케이션을 Kotlin + Spring Boot로 리팩토링 하고 추가 요구사항을 구현합니다. 이 과정에서 Junit5, SQL, JPA, Querydsl 을 사용할 뿐 아니라, 설계 및 구현 관점에서 다양한 방법의 장단점을 다루게 됩니다.',
-  author: '니콜라스',
-  price: '2',
-  thumbnailURL: '/329248-eng2.png',
-  youtubeURL: 'https://www.youtube.com/embed/lslNMgWstRQ',
+const { Title, Text } = Typography;
+
+type Course = {
+  lecture_id: string;
+  lecture_title: string;
+  lecture_summary: string;
+  lecture_url: string;
+  class_id: string;
 };
-
-export default function details() {
-  const { Title, Text } = Typography;
+export default function ClassDetails({ course }: { course: Course }) {
+  const router = useRouter();
   return (
     <section>
+      <Head>
+        <title>{course.lecture_title}</title>
+      </Head>
       <Space style={{ justifyContent: 'space-between', width: '100%' }}>
-        <Space>
-          <EyeOutlined style={{ fontSize: '24px' }} />
-          <PageHeader style={{ paddingLeft: 0 }} title={dummyCourses.title} backIcon={true} />
-        </Space>
+        <PageHeader
+          onBack={() => router.back()}
+          style={{ paddingLeft: 0, width: '100%' }}
+          title={course.lecture_title}
+        />
       </Space>
-      <Space
-        style={{ position: 'relative', width: '100%', paddingBottom: '56.25%', marginBottom:'20px' }}
-        direction="vertical"
-      >
-        <iframe
-          width="100%"
-          style={{ position: 'absolute', width: '100%', height: '100%' }}
-          // height="800px"
-          src={dummyCourses.youtubeURL}
-          title={dummyCourses.title}
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        ></iframe>
-      </Space>
-      <Title level={3}>{dummyCourses.title}</Title>
-      <Text style={{display:'block', fontSize: '18px', marginBottom:'16px'}}>{dummyCourses.summary}</Text>
-      <Button href='/courses' size='middle'>← 돌아가기</Button>
+
+      <PlayerWrapper>
+        <ReactPlayer
+          className="react-player"
+          url={course.lecture_url}
+          controls={true}
+          width={'100%'}
+          height={'100%'}
+        />
+      </PlayerWrapper>
+
+      <Title level={3}>{course.lecture_title}</Title>
+      <Text style={{ display: 'block', fontSize: '18px', marginBottom: '16px' }}>
+        {course.lecture_summary}
+      </Text>
     </section>
   );
 }
+
+const PlayerWrapper = styled.div`
+  position: relative;
+  padding-top: 25px;
+  padding-bottom: 56.25% /* Player ratio: 100 / (1280 / 720) */;
+  .react-player {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+`;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const course = ctx.query;
+  return {
+    props: { course },
+  };
+};
