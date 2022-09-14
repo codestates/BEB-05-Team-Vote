452f1843f123ee5aae356f4dd2a6be6f8e39c733
thyoondev <th.yoon.dev@gmail.com>
2022-09-14 15:47:05 +0900

    fix : broken image

--- client/havruta_dao/pages/courses/index.tsx
+++ client/havruta_dao/pages/courses/index.tsx
@@ -48,6 +48,7 @@ export default function details({ courses }: { courses: Array<Courses> }) {
                 src={`${course.lecture_image}`}
                 alt={course.lecture_title}
                 preview={false}
+                fallback="https://images.unsplash.com/photo-1534337621606-e3df5ee0e97f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80"
               />
             </Link>
 
