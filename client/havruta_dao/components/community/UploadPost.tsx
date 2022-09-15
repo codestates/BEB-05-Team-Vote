594590ec4ccd38d4dff2617d9fd310319938a12b
sungman5 <sungman5@gmail.com>
2022-09-15 21:22:23 +0900

    add: post, reply upload & state

--- client/havruta_dao/components/community/UploadPost.tsx
+++ client/havruta_dao/components/community/UploadPost.tsx
@@ -4,39 +4,58 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { PostInterface } from '../../pages';
 import * as Sentry from '@sentry/react';
+import { loginInfoState } from '../../states/loginInfoState';
+import { useRecoilState } from 'recoil';
+import useDidMoundEffect from '../../states/useDidMountEffect';
+import { now } from 'next-auth/client/_utils';
 
 const { TextArea } = Input;
 const { Text } = Typography;
 
-export default function UploadPost() {
-  const [form] = Form.useForm();
-  
-  const [isValue, setIsValue] = useState('');
-  console.log('입력한 내용===', isValue)
+//데이터 전송 후 200토스트
+// export function completeToast() {
+//   window.location.reload(); // 글 전송 후 페이지 새로 고침
+//   return alert('전송이 완료되었습니다!');
+// }
 
-  function publish(){
-    axios.post('http://localhost:8000/article', {
-      user_id : 4,
+export default function UploadPost({isUpload, setIsUpload}:{isUpload:any, setIsUpload:any}) {
+  const [form] = Form.useForm();
+  const [loginInfo, setLoginInfo] = useRecoilState(loginInfoState);
+  const [isValue, setIsValue] = useState('');
+
+  console.log('입력한 내용===', isValue);
+
+  useDidMoundEffect(() => {
+    console.log('입력!', isUpload);
+    try{
+      axios.post(`${process.env.NEXT_PUBLIC_ENDPOINT}/article`, {
+      user_id: loginInfo.user_id,      
       article_content: `${isValue}`,
     })
     .then((res)=>{
-      console.log('상태 리턴', res)
-      completeToast()
-    })
-    .catch((error)=>{
-      Sentry.captureException(error);
-    })
+      console.log('데이터 입력 완료!')
+    });
+    }
+    catch (error) {
+      Sentry.captureException(error);      
+    }    
+  }, [isUpload]);
 
-  }
-
-  //데이터 전송 후 200토스트  
-  function completeToast(){
-    return alert('전송이 완료되었습니다!')
+  function publish() {    
+    const newPost = {
+      article_content: `${isValue}`,
+      created_at: now(),
+      user_id: loginInfo.user_id,
+      user:{
+        user_nickname: loginInfo.user_nickname
+      }
+    };
+    setIsUpload([...isUpload, newPost]);
   }
 
   return (
     <UploadCard style={{ width: '100%' }}>
-      <Text strong>sungman5</Text>
+      <Text strong>{loginInfo.user_nickname}</Text>
       <Form form={form} name="content">
         <Form.Item name="article_content">
           <TextArea
@@ -55,7 +74,7 @@ export default function UploadPost() {
             onClick={() => publish()}
             type="primary"
             shape={'round'}
-            style={{ float: 'right' }}            
+            style={{ float: 'right' }}
           >
             발행하기
           </Button>
--- client/havruta_dao/pages/index.tsx
+++ client/havruta_dao/pages/index.tsx
@@ -1,15 +1,22 @@
-import { AlignCenterOutlined, ArrowRightOutlined } from '@ant-design/icons';
-import { Col, PageHeader, Row, Radio, Typography, Card } from 'antd';
+import {
+  AlignCenterOutlined,
+  ArrowRightOutlined,
+  LikeOutlined,
+  MessageOutlined,
+} from '@ant-design/icons';
+import { Col, PageHeader, Row, Radio, Typography, Card, Space, Button } from 'antd';
 import type { NextPage } from 'next';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import Post from '../components/community/Post';
 import UploadPost from '../components/community/UploadPost';
 import RcmdCourse from '../components/RcmdCourse';
 import * as Sentry from '@sentry/react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
+import { loginInfoState } from '../states/loginInfoState';
+import { useRecoilState } from 'recoil';
+import useDidMoundEffect from '../states/useDidMountEffect';
 
 export interface PostInterface {
   article_id: number;
@@ -17,6 +24,7 @@ export interface PostInterface {
   article_content: string;
   like_count: number;
   comment_count: number;
+  comments: [];
   created_at: string;
   user: {
     user_id: number;
@@ -31,36 +39,96 @@ export interface PostInterface {
 
 const { Text } = Typography;
 
-const Home: NextPage = ({post}:any) => {
-  const [postList, setPostList] = useState([]);
-  console.log('폭스트',post)
+{
+  /*
 
-  const router = useRouter();
-  // console.log(router)
+1. 최근 글 서버 요청 후, 모든 데이터를 state에 할당
+2. 클릭시 state 전체에서 해당 값만 딱 변경. (모든 데이터 유지)
+3. state 변경시 useEffect 활용하여 업데이트
+4. useEffect 업데이트와 함께 DB 업데이트
+
+axios.post(`${process.env.NEXT_PUBLIC_ENDPOINT}/like`,{
+    user_id :user_id,
+    article_id : article_id,
+  })
+
+*/
+}
+
+const Home: NextPage = ({ post }: any) => {
+  const [loginInfo, setLoginInfo] = useRecoilState(loginInfoState);
   
-  // useEffect(() => {
-  //   getRecentPost();   
-  // }, [postList]);
+  //좋아요 데이터 state
+  const likeData = [...post]
+  const [isLike, setIsLike] = useState(likeData);  
+  const [isIndex, setIsIndex] = useState(Number); // 인덱스를 상태로 저장
+  
+  //업로드시 데이터 state
+  const uploadPost = [...post]
+  const [isUpload, setIsUpload] = useState(uploadPost);  
+
+  // 최초 마운트시 데이터 state
+  const mountData = [...post]
+  const [postList, setPostList] = useState(mountData);
+  
+  
+  console.log('로그인함?', loginInfo)
+  console.log('포스트 근황====', uploadPost);    
+
+  function click_like(article_id: number, like_count: number) {
+    let copy = [...isLike];
+    let index:number = copy.findIndex((obj) => obj.article_id === article_id);
+    copy[index].like_count = like_count + 1;
+    setIsIndex(index);
+    setIsLike(copy);
+
+  }
+
+  useDidMoundEffect(() => {
+      console.log('입력!');
+      console.log('로그인유저아이디', loginInfo.user_id);
+      console.log('아티클아이디', isLike[isIndex].article_id);
+      try {
+        axios
+          .post(`${process.env.NEXT_PUBLIC_ENDPOINT}/like`, {
+            user_id: loginInfo.user_id,
+            article_id: post[isIndex].article_id, // 변경된 객체의 인덱스를 정확히 표시해야함.
+          })
+          .then((res) => {
+            console.log('데이터 입력 완료!', res);
+          });
+      } catch (error) {
+        Sentry.captureException(error);
+      }
+    },
+    [isLike]
+  );
+
+  // console.log('회원정보====', loginInfo);
 
   function getRecentPost() {
     axios
-      .get('http://localhost:8000/article/recent')
+      .get(`${process.env.NEXT_PUBLIC_ENDPOINT}/article/recent`)
       .then((result) => {
         console.log('ㅇㅇㅇ', result.data);
         setPostList(result.data);
+      })
+      .then((res)=>{
+        console.log('최신 순 정렬 성공')
       })
       .catch((error) => {
         Sentry.captureException(error);
       });
   }
 
-  function getLikePost() {
+  function getLikePost() {    
     axios
-      .get('http://localhost:8000/article/like')
+      .get(`${process.env.NEXT_PUBLIC_ENDPOINT}/article/like`)
       .then((result) => {
-        console.log('인기 글 불러오기===', result);
         setPostList(result.data);
-        console.log('포스트리스트', postList);
+      })
+      .then((res)=>{
+        console.log('추천 순 정렬 성공')
       })
       .catch((error) => {
         Sentry.captureException(error);
@@ -86,16 +154,51 @@ const Home: NextPage = ({post}:any) => {
               </Radio.Group>
             }
           />
-          <UploadPost />
+          {
+            loginInfo.user_network === 0 ? 
+            <Space align="center" style={{width:'100%', justifyContent:'center', paddingTop:'20px', paddingBottom:'40px'}}>
+              <Text style={{fontSize:'20px'}}>지금 지갑을 연결하고<br/>커뮤니티 활동을 시작하세요</Text>
+            </Space>            
+            : <UploadPost isUpload={isUpload} setIsUpload={setIsUpload} />
+          } 
 
-          {/* {
-            post.map((element:PostInterface)=>{
-              return(
-                <Post article={element}/>
-              )
-            })
-          } */}
-          
+          {isUpload.map((element: PostInterface) => {
+            return (
+              <Link href={`/community/details/${element.article_id}`}>
+                <div key={element.article_id}>
+                  <PostCard>
+                    <Space direction="vertical" size={'large'}>
+                      <Space>
+                        <Text strong>{element.user.user_nickname}</Text>
+                        <Text type="secondary">{element.created_at}</Text>
+                      </Space>
+                      {element.article_content}
+                      <Space>
+                        <Button
+                          type="link"
+                          icon={<LikeOutlined />}
+                          size="small"
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            // click_like(element.article_id, element.user_id, element.like_count);
+                            
+                            click_like(element.article_id, element.like_count);
+                          }}
+                        >
+                          {' '}
+                          {element.like_count}
+                        </Button>
+                        <Button type="link" icon={<MessageOutlined />} size="small">
+                          {' '}
+                          {element.comment_count}
+                        </Button>
+                      </Space>
+                    </Space>
+                  </PostCard>
+                </div>
+              </Link>
+            );
+          })}
         </Col>
         <Col xl={8} xs={0}>
           <PageHeader
@@ -127,25 +230,19 @@ const PostCard = styled(Card)`
   }
 `;
 
-
-
-export async function getServerSideProps(res:any){
+export const getServerSideProps = async () => {
   try {
-    
-    const abc = await axios.get(`${process.env.NEXT_PUBLIC_ENDPOINT}/article/recent`);        
-    const post = abc.data;    
-    console.log(res)
-    return {      
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_ENDPOINT}/article/recent`);
+    const post = res.data;
+    return {
       props: {
-        post
+        post,
       },
     };
   } catch (error) {
     Sentry.captureException(error);
     return { props: {} };
   }
-}
+};
 
 export default Home;
-
-
