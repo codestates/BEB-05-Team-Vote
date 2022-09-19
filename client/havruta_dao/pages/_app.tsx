b40e0ca042ad61f60d26b133f94cdb2b06c7995a
thyoondev <th.yoon.dev@gmail.com>
2022-09-20 05:35:42 +0900

    add : backgorund with three js

--- client/havruta_dao/pages/_app.tsx
+++ client/havruta_dao/pages/_app.tsx
@@ -14,6 +14,7 @@ import Head from 'next/head';
 import { SWRConfig } from 'swr';
 import axios from 'axios';
 import * as Sentry from '@sentry/react';
+import Background from '../components/backgoround';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const router = useRouter();
@@ -109,7 +110,10 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
                   <Spin tip="Loading..." size={'large'} />
                 </Space>
               ) : (
-                <Component {...pageProps} />
+                <>
+                  <Background />
+                  <Component {...pageProps} />
+                </>
               )}
             </LayoutComponent>
           </SWRConfig>
