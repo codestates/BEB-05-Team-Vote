40fafa786087e7837d2913bff4fef763719f6b14
yjjjwww <yjjjwww@naver.com>
2022-09-21 00:59:12 +0900

    add : get userinfo function, point system, test

--- server/src/router/users.ts
+++ server/src/router/users.ts
@@ -1,12 +1,12 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const router = Router();
-const { user_login} = require("../controller/login_controller");
-const { user_logout } = require("../controller/logout_controller");
+const { user_login } = require('../controller/login_controller');
+const { user_logout } = require('../controller/logout_controller');
+const { userInfo } = require('../controller/userinfo_controller');
 
-router.post("/login", user_login);
-router.post("/logout", user_logout);
+router.post('/login', user_login);
+router.post('/logout', user_logout);
+router.get('/userinfo', userInfo);
 
 module.exports = router;
-
-
