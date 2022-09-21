9c4db60fd0f25c533157e8bc6ba94574726b1642
dhjefflee <104472596+dhjefflee@users.noreply.github.com>
2022-09-22 02:14:22 +0900

    Feature/mypage new (#145)
    * add mypage files on the server side  #
    
    * add userinfo_controller.ts
    
    * add new version of user_infor_controller and users.ts files #

--- server/src/controller/userinfo_controller.ts
+++ server/src/controller/userinfo_controller.ts
@@ -23,4 +23,91 @@ module.exports = {
         res.status(500).send('Server Error');
       });
   },
+
+  userArticle: (req: Request, res: Response) => {
+    const user_id = Number(req.query.user_id as String);
+    const userArticleHandler = async (user_id: Number) => {
+      const myUserReadArticle = await prisma.Article.findMany({
+        where: {
+          user_id: user_id,
+        },
+        orderBy: {
+          created_at: 'desc',
+        },
+        include: {
+          user: true,
+        },
+      });
+      console.dir(myUserReadArticle, { depth: null });
+      return myUserReadArticle;
+    };
+    userArticleHandler(user_id)
+      .then(async (result) => {
+        await prisma.$disconnect();
+        res.status(201).json(result);
+      })
+      .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        res.status(500).send('Server Error');
+      });
+  },
+
+  userComment: (req: Request, res: Response) => {
+    const user_id = Number(req.query.user_id as String);
+    const userCommentHandler = async (user_id: Number) => {
+      const allComment = await prisma.Comment.findMany({
+        where: {
+          user_id: user_id,
+        },
+        orderBy: {
+          created_at: 'desc',
+        },
+        include: {
+          user: true,
+        },
+      });
+      console.dir(allComment, { depth: null });
+      return allComment;
+    };
+    userCommentHandler(user_id)
+      .then(async (result) => {
+        await prisma.$disconnect();
+        res.status(201).json(result);
+      })
+      .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        res.status(500).send('Server Error');
+      });
+  },
+
+  userLecture: (req: Request, res: Response) => {
+    const user_id = Number(req.query.user_id as String);
+    const userLectureHandler = async (user_id: Number) => {
+      const allComment = await prisma.Lecture.findMany({
+        where: {
+          user_id: user_id,
+        },
+        orderBy: {
+          created_at: 'desc',
+        },
+        include: {
+          user: true,
+        },
+      });
+      console.dir(allComment, { depth: null });
+      return allComment;
+    };
+    userLectureHandler(user_id)
+      .then(async (result) => {
+        await prisma.$disconnect();
+        res.status(201).json(result);
+      })
+      .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        res.status(500).send('Server Error');
+      });
+  },
 };
