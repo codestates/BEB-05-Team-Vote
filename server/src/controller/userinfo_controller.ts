3fc7cf50c8ecaf7206ad134e7d700fa65c31fac8
dhjefflee <lexwoodtrans@gmail.com>
2022-09-21 16:44:42 +0900

    add userinfo_controller.ts

---
+++ server/src/controller/userinfo_controller.ts
@@ -0,0 +1,113 @@
+import { Request, Response } from "express";
+const prisma = require("../db/index");
+
+module.exports = {
+  userInfo: (req: Request, res: Response) => {
+    const user_id = Number(req.query.user_id as String);
+    const userInfoHandler = async (user_id: Number) => {
+      const userInfo = await prisma.User.findMany({
+        where: {
+          user_id: user_id          
+        },        
+      });
+      return userInfo;
+    };
+    userInfoHandler(user_id)
+      .then(async (result) => {
+        await prisma.$disconnect();
+        res.status(201).json(result);
+      })
+      .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        res.status(500).send('Server Error');
+      });
+  },
+
+  userArticle: (req: Request, res: Response) => {
+    const user_id = Number(req.query.user_id as String);
+    const userArticleHandler = async (user_id: Number) => {
+      const myUserReadArticle = await prisma.User.findMany({
+        where: {
+          user_id: user_id          
+        },
+        orderBy: {
+          created_at: 'desc'
+        },
+        include: {
+          user: true,          
+        },
+      });
+      console.dir(myUserReadArticle, { depth: null });
+      return myUserReadArticle;
+    };
+    userArticleHandler(user_id)
+      .then(async (result) => {
+        await prisma.$disconnect();
+        res.status(201).json(result);
+      })
+      .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        res.status(500).send('Server Error');
+      });
+  },
+  
+  userComment: (req: Request, res: Response) => {
+    const user_id = Number(req.query.user_id as String);
+    const userCommentHandler = async (user_id: Number) => {
+      const allComment = await prisma.Comment.findMany({
+        where: {
+          user_id: user_id,          
+        },
+        orderBy: {
+          created_at: 'desc',
+        },
+        include: {
+          user: true,
+        },
+      });
+      console.dir(allComment, { depth: null });
+      return allComment;
+    };
+    userCommentHandler(user_id)
+      .then(async (result) => {
+        await prisma.$disconnect();
+        res.status(201).json(result);
+      })
+      .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        res.status(500).send('Server Error');
+      });
+  },
+
+  userLecture: (req: Request, res: Response) => {
+    const user_id = Number(req.query.user_id as String);
+    const userLectureHandler = async (user_id: Number) => {
+      const allComment = await prisma.Comment.findMany({
+        where: {
+          user_id: user_id          
+        },
+        orderBy: {
+          created_at: 'desc',
+        },
+        include: {
+          user: true,
+        },
+      });
+      console.dir(allComment, { depth: null });
+      return allComment;
+    };
+    userLectureHandler(user_id)
+      .then(async (result) => {
+        await prisma.$disconnect();
+        res.status(201).json(result);
+      })
+      .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        res.status(500).send('Server Error');
+      });
+  },
+};
