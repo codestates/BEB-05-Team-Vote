aa805c7cfed0090b0625a259088209b0f992a339
thyoondev <th.yoon.dev@gmail.com>
2022-09-23 04:36:38 +0900

    add : connet to contract #13 #20

--- client/havruta_dao/next.config.js
+++ client/havruta_dao/next.config.js
@@ -31,6 +31,19 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      net: false,
+      stream: require.resolve('stream-browserify'),
+      crypto: require.resolve('crypto-browserify'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      os: require.resolve('os-browserify/browser'),
+    };
+    return config;
+  },
 };
 
 module.exports = withSentryConfig(
